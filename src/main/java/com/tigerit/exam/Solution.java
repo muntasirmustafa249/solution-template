283f3c4384ede687c7e6ae198f7d41558553cae3
Muntasir Mustafa <muntasirmustafa249@yahoo.com>
2016-04-13 08:14:34 +0600

    Solution
    Solution to the given problem

--- src/main/java/com/tigerit/exam/Solution.java
+++ src/main/java/com/tigerit/exam/Solution.java
@@ -3,6 +3,13 @@ package com.tigerit.exam;
 
 import static com.tigerit.exam.IO.*;
 
+
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Objects;
+
 /**
  * All of your application logic should be placed inside this class.
  * Remember we will load your application from our custom container.
@@ -15,12 +22,168 @@ public class Solution implements Runnable {
         // your application entry point
 
         // sample input process
-        String string = readLine();
+        //String string = readLine();
 
-        Integer integer = readLineAsInteger();
+        //Integer integer = readLineAsInteger();
 
         // sample output process
-        printLine(string);
-        printLine(integer);
+        //printLine(string);
+        //printLine(integer);
+        
+        solve();
+        
+    }
+    
+    public void solve()
+    {
+        int testCount;
+        
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        testCount = Integer.parseInt(readInput(input)); //input.nextInt();
+        
+        for(int i = 1;i <= testCount;i++)
+        {
+            int tableCount = Integer.parseInt(readInput(input)); //input.nextInt();
+            
+            Table[] tables = new Table[tableCount];
+            
+            for(int j = 0;j < tableCount;j++)
+            {
+                tables[j] = new Table();
+                String tableName = readInput(input);
+                tables[j].setTableName(tableName);
+                
+                String[] columnAndRow = readInput(input).split(" ", 2);
+                
+                int columnNum = Integer.parseInt(columnAndRow[0]);
+                int rowNum = Integer.parseInt(columnAndRow[1]);
+                tables[j].setColumnNum(columnNum);
+                tables[j].setRowNum(rowNum);
+                
+                String[] columnNames = readInput(input).split(" ", columnNum);
+                System.arraycopy(columnNames, 0, tables[j].columnNames, 0, columnNum);
+                
+                tables[j].data = new ArrayList<int[]>();
+                
+                for(int k = 0;k < rowNum;k++)
+                {
+                    String[] rowStr = readInput(input).split(" ", columnNum);
+                    int[] row = new int[columnNum];
+                    for(int l = 0;l < columnNum;l++)
+                    {
+                        row[l] = Integer.parseInt(rowStr[l]);
+                    }
+                    tables[j].data.add(row);
+                }
+                
+                //tables[j] = tables[j].sortData(tables[j]);
+            }
+            
+            int queryNumber = Integer.parseInt(readInput(input));
+            
+            System.out.println("Test: "+i);
+            
+            for(int j = 1;j <= queryNumber;j++)
+            {
+                String query = "";
+                for(int k = 0;k < 4;k++)
+                {
+                    query = query + " " + readInput(input);
+                }
+                readInput(input);
+                
+                String[] queryParts = query.split("\\s*FROM \\s*");
+                
+                String selectPart = queryParts[0].substring(7).trim(); // required
+                
+                String fromPart = queryParts[1].trim();
+                
+                String[] fromParts = fromPart.split("\\s*ON \\s*");
+                String joinTable = fromParts[0].trim();
+                String condition = fromParts[1].trim(); // required
+                
+                String[] joinTables = joinTable.split("\\s*JOIN \\s*");
+                String table1Name = joinTables[0].trim();
+                String table2Name = joinTables[1].trim();
+                
+                Table table1 = new Table();
+                Table table2 = new Table();
+                
+                String[] table1Names = table1Name.split(" "); // required
+                String[] table2Names = table2Name.split(" "); // required
+                
+                for(int k = 0;k < tableCount;k++)
+                {
+                    if(Objects.equals(tables[k].getTableName(), table1Names[0]))
+                    {
+                        table1 = new Table(tables[k]);
+                        
+                        if(table1Names.length == 2)
+                        {
+                            table1.setAlias(table1Names[1]);
+                        }
+                        break;
+                    }
+                }
+                for(int k = 0;k < tableCount;k++)
+                {
+                    if(Objects.equals(tables[k].getTableName(), table2Names[0]))
+                    {
+                        table2 = new Table(tables[k]);
+                        
+                        if(table2Names.length == 2)
+                        {
+                            table2.setAlias(table2Names[1]);
+                        }
+                        break;
+                    }
+                }
+                
+                Table crossResult = table1.crossJoin(table1, table2);
+                Table innerJoinResult = crossResult.innerJoin(crossResult, condition);
+                Table finalResult = innerJoinResult.select(innerJoinResult, selectPart);
+                
+                String line;
+                
+                line = finalResult.columnNames[0];
+                
+                for(int l = 1;l < finalResult.getColumnNum();l++)
+                {
+                    line = line + " " + finalResult.columnNames[l];
+                }
+                
+                System.out.println(line);
+                
+                for(int m = 0;m < finalResult.getRowNum();m++)
+                {
+                    int[] row = finalResult.data.get(m);
+                    line = "" + row[0];
+                    for(int n = 1;n < finalResult.getColumnNum();n++)
+                    {
+                        line = line + " " + row[n];
+                    }
+                    System.out.println(line);
+                }
+                
+                System.out.println();
+                
+            }
+        }
+    }
+    
+    public static String readInput(BufferedReader reader)
+    {
+        String text;
+        
+        try
+        {
+            text = reader.readLine();
+        }
+        catch(IOException e)
+        {
+            text = null;
+        }
+        
+        return text;
     }
 }
---
+++ src/main/java/com/tigerit/exam/Table.java
@@ -0,0 +1,270 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tigerit.exam;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Objects;
+
+/**
+ *
+ * @author Muntasir
+ */
+public class Table{
+    private String tableName;
+    private String alias;
+    private int rowNum;
+    private int columnNum;
+    public String[] columnNames;
+    public ArrayList<int[]> data;
+    
+    public Table(){}
+    public Table(Table table) // clone constructor
+    {
+        this.tableName = table.getTableName();
+        this.rowNum = table.rowNum;
+        this.columnNum = table.columnNum;
+        
+        this.columnNames = new String[this.columnNum];
+        System.arraycopy(table.columnNames, 0, this.columnNames, 0, this.columnNum);
+        
+        this.data = (ArrayList<int[]>) table.data.clone();
+    }
+    
+    public void setTableName(String tableName)
+    {
+        this.tableName = tableName;
+    }
+    public void setAlias(String alias)
+    {
+        this.alias = alias;
+    }
+    public void setRowNum(int rowNum)
+    {
+        this.rowNum = rowNum;
+    }
+    public void setColumnNum(int columnNum)
+    {
+        this.columnNum = columnNum;
+        this.columnNames = new String[this.columnNum];
+    }
+    public String getTableName()
+    {
+        return tableName;
+    }
+    public String getAlias()
+    {
+        return alias;
+    }
+    public int getRowNum()
+    {
+        return rowNum;
+    }
+    public int getColumnNum()
+    {
+        return columnNum;
+    }
+    
+    public Table crossJoin(Table table1, Table table2)
+    {
+        Table result = new Table();
+        
+        int resultColumnNumber = table1.getColumnNum()+table2.getColumnNum();
+        int resultRowNumber = table1.getRowNum()+table2.getRowNum();
+        
+        result.setColumnNum(resultColumnNumber);
+        result.setRowNum(resultRowNumber);
+        
+        result.data = new ArrayList<int[]>();
+        
+        String[] table1ColumnNames = table1.columnNames;
+        String[] table2ColumnNames = table2.columnNames;
+        
+        for(int i = 0;i < table1ColumnNames.length;i++)
+        {
+            if(table1.getAlias() == null)
+                table1ColumnNames[i] = table1.getTableName()+"."+table1ColumnNames[i];
+            else
+                table1ColumnNames[i] = table1.getAlias()+"."+table1ColumnNames[i];
+        }
+        for(int i = 0;i < table2ColumnNames.length;i++)
+        {
+            if(table2.getAlias() == null)
+                table2ColumnNames[i] = table2.getTableName()+"."+table2ColumnNames[i];
+            else
+                table2ColumnNames[i] = table2.getAlias()+"."+table2ColumnNames[i];
+        }
+        
+        System.arraycopy(table1ColumnNames, 0, result.columnNames, 0, table1ColumnNames.length);
+        System.arraycopy(table2ColumnNames, 0, result.columnNames, table1ColumnNames.length, table2ColumnNames.length);
+        
+        for(int i = 0;i < table1.data.size();i++)
+        {
+            int[] table1Row = table1.data.get(i);
+            for(int j = 0;j < table2.data.size();j++)
+            {
+                int[] table2Row = table2.data.get(j);
+                
+                int[] resultRow = new int[table1Row.length + table2Row.length];
+                System.arraycopy(table1Row, 0, resultRow, 0, table1Row.length);
+                System.arraycopy(table2Row, 0, resultRow, table1Row.length, table2Row.length);
+                
+                result.data.add(resultRow);
+            }
+        }
+        
+        return result;
+    }
+    
+    public Table sortData(Table table)
+    {
+        int rowNum = table.getRowNum();
+        int columnNum = table.getColumnNum();
+        
+        for(int c = 0;c < (rowNum - 1);c++)
+        {
+            for(int d = 0;d < (rowNum - c - 1);d++)
+            {
+                int[] row1 = table.data.get(c);
+                int[] row2 = table.data.get(d);
+                for(int x = 0;x < columnNum;x++)
+                {
+                    if(row1[x] > row2[x])
+                    {
+                        Collections.swap(table.data, c, d);
+                        break;
+                    }
+                    else if(row1[x] < row2[x])
+                        break;
+                }
+            }
+        }
+        
+        return table;
+    }
+    
+    public Table innerJoin(Table table, String condition)
+    {
+        Table result = new Table();
+        result.data = new ArrayList<int[]>();
+        
+        result.setColumnNum(table.getColumnNum());
+        result.columnNames = table.columnNames;
+        int resultRowCount = 0;
+        
+        String[] conditionParts = condition.split("=");
+        String conditionPart1 = conditionParts[0].trim();
+        String conditionPart2 = conditionParts[1].trim();
+        
+        int conditionPart1Address = -1, conditionPart2Address = -1;
+        
+        for(int i = 0, done = 0;i < table.columnNames.length;i++)
+        {
+            if(Objects.equals(table.columnNames[i], conditionPart1))
+            {
+                conditionPart1Address = i;
+                done++;
+            }
+            if(Objects.equals(table.columnNames[i], conditionPart2))
+            {
+                conditionPart2Address = i;
+                done++;
+            }
+            
+            if(done >= 2)
+                break;
+        }
+        
+        if(conditionPart1Address < 0 || conditionPart2Address < 0)
+        {
+            //System.err.println("JOIN condition invalid");
+            //System.exit(1); // invalid query
+        }
+        
+        for(int i = 0;i < table.data.size();i++)
+        {
+            int[] row = table.data.get(i);
+            
+            if(row[conditionPart1Address] == row[conditionPart2Address])
+            {
+                result.data.add(row);
+                resultRowCount++;
+            }
+        }
+        
+        result.setRowNum(resultRowCount);
+        
+        return result;
+    }
+    
+    public Table select(Table table, String selectPart)
+    {
+        Table result = new Table();
+        
+        if(Objects.equals(selectPart, "*")) // in the case of "SELECT *"
+        {
+            result = new Table(table);
+            
+            for(int i = 0;i < result.getColumnNum();i++)
+            {
+                String[] columnNameParts = result.columnNames[i].split("\\.");
+                result.columnNames[i] = columnNameParts[1];
+            }
+        }
+        else
+        {
+            String[] selectParts = selectPart.split(",");
+            int resultColumnNum = selectParts.length;
+            
+            result.setRowNum(table.getRowNum());
+            result.setColumnNum(resultColumnNum);
+            
+            result.columnNames = new String[resultColumnNum];
+            
+            for(int i = 0;i < resultColumnNum;i++)
+            {
+                result.columnNames[i] = selectParts[i].trim();
+            }
+            
+            int[] columnAddress = new int[resultColumnNum];
+            
+            for(int i = 0;i < resultColumnNum;i++)
+            {
+                for(int j = 0;j < table.getColumnNum();j++)
+                {
+                    if(Objects.equals(result.columnNames[i], table.columnNames[j]))
+                    {
+                        columnAddress[i] = j;
+                        break;
+                    }
+                }
+            }
+            
+            result.data = new ArrayList<int[]>();
+            int resultRowNum = result.getRowNum();
+            
+            for(int i = 0;i < resultRowNum;i++)
+            {
+                int[] row = new int[resultColumnNum];
+                for(int j = 0;j < resultColumnNum;j++)
+                {
+                    row[j] = table.data.get(i)[columnAddress[j]];
+                }
+                result.data.add(row);
+            }
+            
+            for(int i = 0;i < result.getColumnNum();i++)
+            {
+                String[] columnNameParts = result.columnNames[i].split("\\.");
+                result.columnNames[i] = columnNameParts[1];
+            }
+        }
+        
+        result = result.sortData(result);
+        
+        return result;
+    }
+}
